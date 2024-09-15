4e3db847a4bb4aeb2a0cf01f0a018694abe9fc8d
Clinton Selke <clinuxrulz@gmail.com>
2024-09-16 09:57:29 +1000

    fine grain reactivity signal

--- src/fgr.rs
+++ src/fgr.rs
@@ -2,6 +2,64 @@ use std::borrow::Borrow;
 use std::rc::Rc;
 use std::cell::{Ref, RefCell, RefMut};
 
+pub struct FrgCtx {
+    stack: Vec<NodeRef>,
+    tmp_buffer_1: Vec<NodeRef>,
+    tmp_buffer_2: Vec<NodeRef>,
+}
+
+pub struct Signal<A> {
+    impl_: Rc<RefCell<SignalImpl<A>>>,
+}
+
+impl<A: 'static> Into<NodeRef> for &Signal<A> {
+    fn into(self) -> NodeRef {
+        let node = Rc::clone(&self.impl_);
+        NodeRef {
+            node,
+        }
+    }
+}
+
+
+struct SignalImpl<A> {
+    node_data: NodeData,
+    value: A,
+    value_changed: bool,
+}
+
+impl<A> IsNode for SignalImpl<A> {
+    fn node_data(&self) -> &NodeData {
+        &self.node_data
+    }
+
+    fn node_data_mut(&mut self) -> &mut NodeData {
+        &mut self.node_data
+    }
+
+    fn update(&mut self) -> bool {
+        let result = self.value_changed;
+        self.value_changed = false;
+        result
+    }
+}
+
+impl<A: 'static> Signal<A> {
+    pub fn value<'a>(&'a self) -> Ref<'_, A> {
+        let impl_ = (*self.impl_).borrow();
+        let val = Ref::map(impl_, |impl_| &impl_.value);
+        val
+    }
+
+    pub fn update_value<CALLBACK: FnOnce(&mut A)>(&mut self, fgr_ctx: &mut FrgCtx, callback: CALLBACK) {
+        let mut impl_ = (*self.impl_).borrow_mut();
+        callback(&mut impl_.value);
+        impl_.value_changed = true;
+        fgr_ctx.stack.push((&*self).into());
+        propergate_dependents_flags_to_stale(fgr_ctx);
+    }
+}
+
 #[derive(PartialEq, Eq, Clone, Copy)]
 enum NodeFlag {
     Ready,
@@ -68,8 +126,10 @@ impl NodeRef {
     }
 }
 
-
-fn update_graph(tmp_buffer_1: &mut Vec<NodeRef>, tmp_buffer_2: &mut Vec<NodeRef>, stack: &mut Vec<NodeRef>) {
+fn update_graph(fgr_ctx: &mut FrgCtx) {
+    let tmp_buffer_1 = &mut fgr_ctx.tmp_buffer_1;
+    let tmp_buffer_2 = &mut fgr_ctx.tmp_buffer_2;
+    let stack = &mut fgr_ctx.stack;
     loop {
         let Some(node) = stack.pop() else { break; };
         let flag = node.with_node(|n| n.node_data().flag);
@@ -124,5 +184,20 @@ fn update_graph(tmp_buffer_1: &mut Vec<NodeRef>, tmp_buffer_2: &mut Vec<NodeRef>
             let n2 = n.node_data_mut();
             n2.changed = false;
         });
+    }
+}
+
+fn propergate_dependents_flags_to_stale(fgr_ctx: &mut FrgCtx) {
+    loop {
+        let Some(at) = fgr_ctx.stack.pop() else { break; };
+        at.with_node(|n| {
+            for dep in &n.node_data().dependents {
+                fgr_ctx.tmp_buffer_1.push(dep.clone());
+            }
+        });
+        for dep in fgr_ctx.tmp_buffer_1.drain(..) {
+            dep.with_node_mut(|n| n.node_data_mut().flag = NodeFlag::Stale);
+            fgr_ctx.stack.push(dep);
+        }
     }
 }
